84543a5ae10218a5eb72809e5150c70ac296ef13
RaisaBadal5 <raya.badalova@gmail.com>
2024-03-15 22:56:41 +0400

    Initial Commit

---
+++ Homework4/Homework4App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework4App.swift
+//  Homework4
+//
+//  Created by Default on 15.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Homework4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
